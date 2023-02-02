ae19ff19d4ccadbf5a0bdbf4854687eb938ba29c
lcardozo <lcardozo@106-25.info2>
2023-02-02 14:35:04 +0100

    :/

--- FuncionesArrays/src/ejercicios/Ej5.java
+++ FuncionesArrays/src/ejercicios/Ej5.java
@@ -10,7 +10,24 @@ public class Ej5 {
 
 	}
 	public static int[] sinRepetidos(int t[]) {
+		//DEBO HACER LA BUSQUEDA EN LA TABLA VACIAAAAAAAAAAA
 		
+		int pos; 
+		for(int i=0;i<t.length;i++) {
+			t[i]=(int)(Math.random()*100);
+		}
 		
-	}
+	}//fin del metodo
+	
+		public static int[] borrar(int[] tabla, int indice) {
+			if (indice >= 0) {
+				System.arraycopy(tabla, indice + 1, tabla, indice, tabla.length - indice - 1);
+			}
+
+			tabla = Arrays.copyOf(tabla, tabla.length - 1);
+			return tabla;
+		}//fin del metodo
+	
+	
+	
 }
---
+++ FuncionesArrays/src/ejercicios/Ej6.java
@@ -0,0 +1,27 @@
+package ejercicios;
+
+import java.util.Arrays;
+
+public class Ej6 {
+/*Escribir la función: int[] eliminarMayores(int t[], int valor), 
+ * que crea y devuelve una copia de la tabla t donde se han eliminado todos los elementos que son mayores que valor.*/
+	public static void main(String[] args) {
+		
+
+	}
+	public static int [] eliminarMayores(int t[], int valor) {
+		
+		
+		
+		/*
+		for (int i=0; i<t.length;i++ ) {
+			if (t[i]> valor { 
+				System.arraycopy(t,i+1,t,i,t.length-i-1);
+				
+				t=Arrays.copyOf(t, t.length-1);
+		}//fin del if
+			return t[];
+		}//fin del for
+		*/
+	}//fin del metodo
+}
