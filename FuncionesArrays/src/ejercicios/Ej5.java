e8b4d7b22d5c99d1dd1e6463987b53f143d6e0a6
lcardozo <lcardozo@106-25.info2>
2023-02-02 13:45:44 +0100

    ???

---
+++ FuncionesArrays/src/ejercicios/Ej5.java
@@ -0,0 +1,16 @@
+package ejercicios;
+
+import java.util.Arrays;
+
+public class Ej5 {
+/*Implementar la función: int[] sinRepetidos(int t[]), que construye y devuelve una tabla de la longitud apropiada, con los elementos de t, 
+ * donde se han eliminado los datos repetidos.*/
+	public static void main(String[] args) {
+		
+
+	}
+	public static int[] sinRepetidos(int t[]) {
+		
+		
+	}
+}
