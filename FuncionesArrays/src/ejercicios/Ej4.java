be1d0a19c92b710cb53f11ffcf50c66e45fbcfe4
lcardozo <lcardozo@106-25.info2>
2023-02-02 10:14:09 +0100

    ej4

---
+++ FuncionesArrays/src/ejercicios/Ej4.java
@@ -0,0 +1,13 @@
+package ejercicios;
+
+public class Ej4 {
+/*Escribe la función: int buscar(int t[], int clave), que busca de forma secuencial en la tabla t el valor clave.
+ *  En caso de encontrarlo, devuelve en qué posición lo encuentra; y en caso contrario, devolverá -1.*/
+	public static void main(String[] args) {
+		
+
+	}//fin del main
+ public static int buscar(int t[], int clave) {
+	 
+ }
+}
